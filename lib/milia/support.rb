898e4e4b41f0d8e569847d1ed02ecbcbe366bd02
dasibre <james.naadjie@gmail.com>
2019-01-21 11:15:34 -0500

    add uuid regx check

--- lib/milia/support.rb
+++ lib/milia/support.rb
@@ -4,9 +4,11 @@ module Milia
     # Validate the content and type of the given tenant_id. Currently
     # Integers and Strings (UUID) are supported.
     def self.valid_tenant_id?(tenant_id)
+      uuid_regex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
       return false if tenant_id.blank?
       return false if tenant_id.kind_of?(Integer) && tenant_id.zero?
       return false if !(tenant_id.kind_of?(Integer) || tenant_id.kind_of?(String))
+      return false if (tenant_id.kind_of?(String) && uuid_regex.match?(tenant_id))
       true
     end
 
